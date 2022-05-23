72a627dff45fbc0a1f693884133da501e377ab18
Mari <mariab1@list.ru>
2022-05-23 19:21:51 +0300

    di-java added

--- src/main/java/ru/netology/servlets/MainServlet.java
+++ src/main/java/ru/netology/servlets/MainServlet.java
@@ -1,22 +1,21 @@
 package ru.netology.servlets;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.JavaConfig;
 import ru.netology.controller.PostController;
-import ru.netology.repository.PostRepository;
-import ru.netology.service.PostService;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class MainServlet extends HttpServlet {
     private PostController controller;
 
     @Override
     public void init() {
-        final var repository = new PostRepository();
-        final var service = new PostService(repository);
-        controller = new PostController(service);
+        final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+
+        controller = context.getBean(PostController.class);
     }
 
     @Override
